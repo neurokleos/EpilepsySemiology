5cbfbc536204f5018f5cd49ee9107ad9d7c56d7a
Fernando Perez-Garcia <fepegar@gmail.com>
2019-12-19 01:33:06 +0100

    Update code

--- SemiologyVisualization/SemiologyVisualization.py
+++ SemiologyVisualization/SemiologyVisualization.py
@@ -1,5 +1,7 @@
 import csv
+import logging
 from pathlib import Path
+from abc import ABC, abstractmethod
 
 import numpy as np
 import SimpleITK as sitk
@@ -7,6 +9,9 @@ import SimpleITK as sitk
 import vtk, qt, ctk, slicer
 import sitkUtils as su
 from slicer.ScriptedLoadableModule import *
+
+
+WHITE = 1, 1, 1
 
 #
 # SemiologyVisualization
@@ -39,10 +44,26 @@ class SemiologyVisualizationWidget(ScriptedLoadableModuleWidget):
 
   def setup(self):
     ScriptedLoadableModuleWidget.setup(self)
-    self.semiologiesDict = self.getSemiologiesDict(semiologies)
+    self.logic = SemiologyVisualizationLogic()
+    self.semiologiesDict = self.logic.getSemiologiesDict(semiologies)
+    self.parcellation = GIFParcellation(
+      segmentationPath=self.logic.getGifSegmentationPath(),
+      colorTablePath=self.logic.getGifTablePath(),
+    )
     self.makeGUI()
+    slicer.semiologyVisualization = self
 
   def makeGUI(self):
+    self.makeDominantSideButton()
+    self.makeEzSideButton()
+    self.makeSemiologiesButton()
+    self.makeLoadDataButton()
+    self.makeApplyButton()
+
+    # Add vertical spacer
+    self.layout.addStretch(1)
+
+  def makeDominantSideButton(self):
     dominantSideGroupButton = qt.QGroupBox('Dominant side')
     self.leftDominantRadioButton = qt.QRadioButton('Left')
     self.leftDominantRadioButton.setChecked(True)
@@ -52,6 +73,7 @@ class SemiologyVisualizationWidget(ScriptedLoadableModuleWidget):
     dominantSideLayout.addWidget(self.rightDominantRadioButton)
     self.layout.addWidget(dominantSideGroupButton)
 
+  def makeEzSideButton(self):
     ezSideGroupButton = qt.QGroupBox('Epileptogenic zone side')
     self.leftEzRadioButton = qt.QRadioButton('Left')
     self.leftEzRadioButton.setChecked(True)
@@ -61,6 +83,7 @@ class SemiologyVisualizationWidget(ScriptedLoadableModuleWidget):
     ezSideLayout.addWidget(self.rightEzRadioButton)
     self.layout.addWidget(ezSideGroupButton)
 
+  def makeSemiologiesButton(self):
     semiologiesCollapsibleButton = ctk.ctkCollapsibleButton()
     semiologiesCollapsibleButton.text = 'Semiologies'
     self.layout.addWidget(semiologiesCollapsibleButton)
@@ -82,19 +105,20 @@ class SemiologyVisualizationWidget(ScriptedLoadableModuleWidget):
 
     # semiologiesFormLayout.addWidget(self.getSemiologiesWidget())
 
-    self.applyButton = qt.QPushButton("Apply")
-    self.applyButton.toolTip = "Run the algorithm."
-    self.applyButton.enabled = False
-    semiologiesFormLayout.addRow(self.applyButton)
-
-    # connections
     self.parcellationPathEdit.currentPathChanged.connect(self.onSelect)
     self.referencePathEdit.currentPathChanged.connect(self.onSelect)
     self.scoresPathEdit.currentPathChanged.connect(self.onSelect)
-    self.applyButton.clicked.connect(self.onApplyButton)
 
-    # Add vertical spacer
-    self.layout.addStretch(1)
+  def makeLoadDataButton(self):
+    self.loadDataButton = qt.QPushButton('Load data')
+    self.loadDataButton.clicked.connect(self.onLoadDataButton)
+    self.layout.addWidget(self.loadDataButton)
+
+  def makeApplyButton(self):
+    self.applyButton = qt.QPushButton('Apply')
+    self.applyButton.enabled = False
+    self.applyButton.clicked.connect(self.onApplyButton)
+    self.layout.addWidget(self.applyButton)
 
   def getSemiologiesWidget(self):
     semiologiesWidget = qt.QWidget()
@@ -109,15 +133,16 @@ class SemiologyVisualizationWidget(ScriptedLoadableModuleWidget):
       semiologiesLayout.addWidget(widgetsDict['rightCheckBox'], row, 2)
     return semiologiesWidget
 
-  def getSemiologiesDict(self, semiologies):
-    semiologiesDict = {}
-    for semiology in semiologies:
-      semiologiesDict[semiology] = dict(
-        leftCheckBox=qt.QCheckBox(),
-        rightCheckBox=qt.QCheckBox(),
-      )
-    return semiologiesDict
+  def updateColors(self):
+    self.parcellation.setRandomColors()
 
+    # if self.useGifColors:
+    #   parcellation.setOriginalColors()
+    # else:
+    #   scores = self.getScores()
+    #   parcellation.setScoresColors(scores)
+
+  # Slots
   def onSelect(self):
     # parcellationPath = Path(self.parcellationPathEdit.currentPath)
     # referencePath = Path(self.referencePathEdit.currentPath)
@@ -135,32 +160,40 @@ class SemiologyVisualizationWidget(ScriptedLoadableModuleWidget):
       print(scoresIsFile, 'does not exist')
     self.applyButton.enabled = scoresIsFile
 
-  def onApplyButton(self):
-    # self.parcellationPathEdit.addCurrentPathToHistory()
-    # self.referencePathEdit.addCurrentPathToHistory()
-    self.scoresPathEdit.addCurrentPathToHistory()
-
+  def onLoadDataButton(self):
     logic = SemiologyVisualizationLogic()
     self.referenceVolumeNode = slicer.util.loadVolume(
-      str(logic.getDefaultReferencePath()))
-    self.parcellationVolumeNode = logic.loadParcellation(
-      logic.getDefaultParcellationPath())
+        str(logic.getDefaultReferencePath()))
+    self.parcellation.load()
+    self.applyButton.enabled = True
+
+  def onApplyButton(self):
+    self.updateColors()
+    # # self.parcellationPathEdit.addCurrentPathToHistory()
+    # # self.referencePathEdit.addCurrentPathToHistory()
+    # self.scoresPathEdit.addCurrentPathToHistory()
+
+    # logic = SemiologyVisualizationLogic()
     # self.referenceVolumeNode = slicer.util.loadVolume(
-    #   self.referencePathEdit.currentPath)
+    #   str(logic.getDefaultReferencePath()))
     # self.parcellationVolumeNode = logic.loadParcellation(
-    #   self.parcellationPathEdit.currentPath)
-    # self.parcellationSegmentationNode = logic.labelMapToSegmentation(
-    #   self.parcellationVolumeNode)
-    self.scoresNode = logic.getScoresVolumeNode(
-      self.scoresPathEdit.currentPath,
-      self.parcellationVolumeNode,
-    )
-    slicer.util.setSliceViewerLayers(
-      foreground=self.scoresNode,
-      foregroundOpacity=1,
-      labelOpacity=0,
-    )
-    self.scoresNode.GetDisplayNode().SetInterpolate(False)
+    #   logic.getDefaultParcellationPath())
+    # # self.referenceVolumeNode = slicer.util.loadVolume(
+    # #   self.referencePathEdit.currentPath)
+    # # self.parcellationVolumeNode = logic.loadParcellation(
+    # #   self.parcellationPathEdit.currentPath)
+    # # self.parcellationSegmentationNode = logic.labelMapToSegmentation(
+    # #   self.parcellationVolumeNode)
+    # self.scoresNode = logic.getScoresVolumeNode(
+    #   self.scoresPathEdit.currentPath,
+    #   self.parcellationVolumeNode,
+    # )
+    # slicer.util.setSliceViewerLayers(
+    #   foreground=self.scoresNode,
+    #   foregroundOpacity=1,
+    #   labelOpacity=0,
+    # )
+    # self.scoresNode.GetDisplayNode().SetInterpolate(False)
 
 
 #
@@ -168,6 +201,15 @@ class SemiologyVisualizationWidget(ScriptedLoadableModuleWidget):
 #
 
 class SemiologyVisualizationLogic(ScriptedLoadableModuleLogic):
+
+  def getSemiologiesDict(self, semiologies):
+    semiologiesDict = {}
+    for semiology in semiologies:
+      semiologiesDict[semiology] = dict(
+        leftCheckBox=qt.QCheckBox(),
+        rightCheckBox=qt.QCheckBox(),
+      )
+    return semiologiesDict
 
   def loadParcellation(self, imagePath, gifVersion=None):
     volumeNode = slicer.util.loadLabelVolume(str(imagePath))
@@ -179,17 +221,27 @@ class SemiologyVisualizationLogic(ScriptedLoadableModuleLogic):
   def labelMapToSegmentation(self, labelMapNode):
     pass
 
-  def getGifColorNode(self, version=None):
+  def getGifTablePath(self, version=None):
     version = 3 if version is None else version
     colorDir = self.getResourcesDir() / 'Color'
     filename = f'BrainAnatomyLabelsV{version}_0.txt'
     colorPath = colorDir / filename
+    return colorPath
+
+  def getGifSegmentationPath(self):
+    return self.getImagesDir() / 'MNI_152_gif_cerebrum.seg.nrrd'
+
+  def getGifColorNode(self, version=None):
+    colorPath = self.getGifTablePath(version=version)
     colorNodeName = colorPath.stem
     className = 'vtkMRMLColorTableNode'
     colorNode = slicer.util.getFirstNodeByClassByName(className, colorNodeName)
     if colorNode is None:
       colorNode = slicer.util.loadColorTable(str(colorPath))
     return colorNode
+
+  def getGifSegmentationNode(self):
+    return slicer.util.loadSegmentation(str(self.getGifSegmentationPath()))
 
   def getResourcesDir(self):
     moduleDir = Path(slicer.util.modulePath(self.moduleName)).parent
@@ -241,6 +293,17 @@ class SemiologyVisualizationLogic(ScriptedLoadableModuleLogic):
     image.SetSpacing(referenceImage.GetSpacing())
     return image
 
+  def readScores(self, scoresPath):
+    with open(scoresPath) as csvfile:
+      reader = csv.reader(csvfile)
+      next(reader)  # assume there is a header row
+      scoresDict = {int(label): float(score) for (label, score) in reader}
+    return scoresDict
+
+  def getTestScores(self):
+    scoresPath = self.getResourcesDir() / 'Test' / 'head.csv'
+    return self.readScores(scoresPath)
+
 
 class SemiologyVisualizationTest(ScriptedLoadableModuleTest):
   """
@@ -290,13 +353,198 @@ class SemiologyVisualizationTest(ScriptedLoadableModuleTest):
     self.delayDisplay('Test passed!')
 
 
-LOREN_IPSUM = """Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse magna purus, accumsan non laoreet id, commodo eget ligula. Vestibulum eros sem, congue non elit at, ornare vestibulum odio. Duis dapibus egestas ultricies. Morbi dignissim sapien a ipsum volutpat, a rhoncus ante finibus. Phasellus consequat et neque at dignissim. Nullam hendrerit elementum accumsan. Phasellus nec lectus id eros rutrum feugiat et eget elit. Curabitur luctus enim a consectetur pharetra. Donec convallis orci arcu, eget tempor orci faucibus non. Curabitur semper congue mattis.
+class Parcellation(ABC):
+  def __init__(self, segmentationPath):
+    self.segmentationPath = Path(segmentationPath)
+    self.segmentationNode = None
+    self._labelMap = None
 
-Morbi facilisis eleifend enim quis dignissim. Interdum et malesuada fames ac ante ipsum primis in faucibus. In eget magna a quam gravida tincidunt. Sed convallis eget purus quis blandit. Integer et nisi ante. Sed ultrices est in neque tempus, eu tristique turpis ullamcorper. Pellentesque pharetra turpis vel mattis vestibulum.
+  # @property
+  # def label_map(self):
+  #   if self._labelMap is None:
+  #     self._labelMap = sitk.ReadImage(str(self.parcellation_path))
+  #     if 'float' in self._labelMap.GetPixelIDTypeAsString():
+  #       self._labelMap = sitk.Cast(self._labelMap, sitk.sitkUInt16)
+  #   return self._labelMap
 
-In finibus, mauris vitae condimentum blandit, urna tortor blandit libero, ac molestie risus velit ac neque. Aliquam ornare sagittis ligula cursus tempus. Maecenas suscipit vulputate imperdiet. Ut blandit placerat porttitor. In maximus, ante quis tempus pretium, sapien neque vehicula diam, semper volutpat dui nisl gravida nunc. Quisque sagittis sit amet massa ut dictum. Quisque dignissim, orci id facilisis fermentum, tortor elit hendrerit lectus, et dictum ante magna non ligula. Fusce vitae pretium augue, porta semper ante. Maecenas porta, purus eu lobortis cursus, enim enim mollis sem, non tempus velit arcu quis augue. Etiam eu leo ipsum.
+  # Note that @property must come before @abstractmethod
+  @property
+  @abstractmethod
+  def colorTable(self):
+    pass
 
-Aenean lorem dolor, congue quis iaculis vel, facilisis a nunc. Mauris accumsan sapien ut sem fermentum ultrices. Praesent volutpat mauris non erat fringilla, accumsan consequat justo rhoncus. Phasellus eu sollicitudin turpis. Curabitur sit amet rutrum purus. Proin ac velit massa. Fusce blandit turpis vel mi commodo, nec posuere lacus accumsan. Nunc est augue, imperdiet nec bibendum at, pulvinar at sapien. Curabitur ac nibh urna. Ut vitae magna mauris.
+  @property
+  def segmentation(self):
+    return self.segmentationNode.GetSegmentation()
 
-Praesent a vestibulum enim, a dignissim dui. In porttitor faucibus orci, sit amet euismod metus gravida ac. Nullam at ante purus. Morbi sed nibh ipsum. Integer imperdiet aliquet est. Fusce eu volutpat purus. Integer ac bibendum mauris, ac porta leo. Nam condimentum venenatis ligula quis iaculis. Donec risus leo, euismod in tincidunt nec, cursus sagittis massa. Pellentesque at mauris ipsum. Etiam luctus at sem at condimentum. Vivamus vestibulum dolor sit amet pretium aliquet.
-"""
+  def getSegmentIDs(self):
+    stringArray = vtk.vtkStringArray()
+    self.segmentation.GetSegmentIDs(stringArray)
+    segmentIDs = [
+        stringArray.GetValue(n)
+        for n in range(stringArray.GetNumberOfValues())
+    ]
+    return segmentIDs
+
+  def getSegments(self):
+    return [self.segmentation.GetSegment(x) for x in self.getSegmentIDs()]
+
+  def load(self):
+    stem = self.segmentationPath.name.split('.')[0]
+    try:
+      node = slicer.util.getNode(stem)
+      logging.info(f'Segmentation found in scene: {stem}')
+    except slicer.util.MRMLNodeNotFoundException:
+      logging.info(f'Segmentation not found in scene: {stem}')
+      node = slicer.util.loadSegmentation(str(self.segmentationPath))
+    self.segmentationNode = node
+
+  def isValidNumber(self, number):
+    return self.colorTable.isValidNumber(number)
+
+  def getColorFromName(self, name):
+    return self.colorTable.getColorFromName(name)
+
+  def getColorFromSegment(self, segment):
+    return self.getColorFromName(segment.GetName())
+
+  def getLabelFromName(self, name):
+    return self.colorTable.getLabelFromName(name)
+
+  def getLabelFromSegment(self, segment):
+    return self.getLabelFromName(segment.GetName())
+
+  def setOriginalColors(self):
+    segments = self.getSegments()
+    numSegments = len(segments)
+    progressDialog = slicer.util.createProgressDialog(
+      value=0,
+      maximum=numSegments,
+      windowTitle='Changing colors...',
+    )
+    for i, segment in enumerate(segments):
+      progressDialog.setValue(i)
+      slicer.app.processEvents()
+      color = self.getColorFromSegment(segment)
+      segment.SetColor(color)
+    progressDialog.setValue(numSegments)
+    slicer.app.processEvents()
+    progressDialog.close()
+
+  def setScoresColors(self, scoresDict, colorNode, defaultOpacity=0.2):
+    segments = self.getSegments()
+    numSegments = len(segments)
+    progressDialog = slicer.util.createProgressDialog(
+      value=0,
+      maximum=numSegments,
+      windowTitle='Changing colors...',
+    )
+    for i, segment in enumerate(segments):
+      progressDialog.setValue(i)
+      slicer.app.processEvents()
+      label = self.getLabelFromSegment(segment)
+      scores = scoresDict.values()
+      minScore = min(scores)
+      maxScore = max(scores)
+      color = WHITE
+      opacity = defaultOpacity
+      if label in scoresDict:
+        score = scoresDict[label]
+        if score > 0:
+          opacity = 1
+          score -= minScore
+          score /= maxScore
+          color = self.getColorFromScore(score, colorNode)
+      segment.SetColor(color)
+      # segment.SetOpacity(opacity)  # need to access to segNode.displayNode
+    progressDialog.setValue(numSegments)
+    slicer.app.processEvents()
+    progressDialog.close()
+
+  def getColorFromScore(self, normalizedScore, colorNode):
+    """This method is very important"""
+    numColors = colorNode.GetNumberOfColors()
+    scoreIndex = int((numColors - 1) * normalizedScore)
+    colorAlpha = 4 * [0]
+    colorNode.GetColor(scoreIndex, colorAlpha)
+    color = np.array(colorAlpha[:3])
+    return color
+
+  def setRandomColors(self):
+    """For debugging purposes"""
+    segments = self.getSegments()
+    numSegments = len(segments)
+    progressDialog = slicer.util.createProgressDialog(
+        value=0,
+        maximum=numSegments,
+        windowTitle='Changing colors...',
+    )
+    for i, segment in enumerate(segments):
+      progressDialog.setValue(i)
+      slicer.app.processEvents()
+      color = self.getRandomColor()
+      segment.SetColor(color)
+    progressDialog.setValue(numSegments)
+    slicer.app.processEvents()
+    progressDialog.close()
+
+  def getRandomColor(self, normalized=True):
+    return np.random.rand(3)
+
+
+class GIFParcellation(Parcellation):
+  def __init__(self, segmentationPath, colorTablePath):
+    Parcellation.__init__(self, segmentationPath)
+    self.colorTablePath = colorTablePath
+    self._colorTable = None
+
+  @property
+  def colorTable(self):
+    return self._colorTable
+
+  def load(self):
+    super().load()
+    self._colorTable = GIFColorTable(self.colorTablePath)
+
+
+class ColorTable(ABC):
+  def __init__(self, path):
+    self.structuresDict = self.readColorTable(path)
+
+  def getStructureNameFromLabelNumber(self, labelNumber):
+    return self.structuresDict[labelNumber]['name']
+
+  def isValidNumber(self, number):
+    return number in self.structuresDict
+
+  @staticmethod
+  def readColorTable(path):
+    structuresDict = {}
+    with open(path) as f:
+      for row in f:
+        label, name, *color, _ = row.split()
+        label = int(label)
+        color = np.array(color, dtype=np.float) / 255
+        structuresDict[label] = dict(name=name, color=color)
+    return structuresDict
+
+  def getColorFromName(self, name):
+    for structureDict in self.structuresDict.values():
+      if structureDict['name'] == name:
+        color = structureDict['color']
+        break
+    else:
+      raise KeyError(f'Structure {name} not found in color table')
+    return color
+
+  def getLabelFromName(self, name):
+    for label, structureDict in self.structuresDict.items():
+      if structureDict['name'] == name:
+        result = label
+        break
+    else:
+      raise KeyError(f'Structure {name} not found in color table')
+    return result
+
+class GIFColorTable(ColorTable):
+  pass
